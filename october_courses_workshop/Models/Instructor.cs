b5628ab5b5d91874b085e688585cfaa1f99cc3d2
edanielbluford <e.daniel.bluford@outlook.com>
2019-10-11 15:10:03 -0400

    added views

---
+++ october_courses_workshop/Models/Instructor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace october_courses_workshop.Models
+{
+    public class Instructor
+    {
+    }
+}
