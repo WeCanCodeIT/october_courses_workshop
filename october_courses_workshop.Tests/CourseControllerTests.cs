fdaf8dc80330a5df975d8a9236d2c1cc015ebef1
edanielbluford <e.daniel.bluford@outlook.com>
2019-10-17 10:28:43 -0400

    Added NSUB branch and refactored Testes

--- october_courses_workshop.Tests/CourseControllerTests.cs
+++ october_courses_workshop.Tests/CourseControllerTests.cs
@@ -6,15 +6,23 @@ using october_courses_workshop.Models;
 using october_courses_workshop.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using october_courses_workshop.Repositories;
+using NSubstitute;
 
 namespace october_courses_workshop.Tests
 {
     public class CourseControllerTests
     {
 
-        CourseController underTest = new CourseController(CoursesRepository);
+        CourseController underTest;
+        IRepository<Courses> coursesRepo;
 
-        public static IRepository<Courses> CoursesRepository { get; private set; }
+        public CourseControllerTests()
+        {
+            coursesRepo = Substitute.For<IRepository<Courses>>();
+            underTest = new CourseController(coursesRepo);
+        }
+
+       
 
         [Fact]
         public void Index_Returns_A_View()
@@ -25,14 +33,34 @@ namespace october_courses_workshop.Tests
         }
 
         [Fact]
-        public void Index_Passes_CourseModel_To_View()
+        public void Index_Passes_All_Courses_To_View()
         {
+            var expectedCourses = new List<Courses>();
+            coursesRepo.GetAll().Returns(expectedCourses);
+
             var result = underTest.Index();
 
-            Assert.IsType<List<Courses>>(result.Model);
+            Assert.Equal(expectedCourses, result.Model);
         }
 
+        [Fact]
+        public void Details_Return_A_View()
+        {
+            var result = underTest.Details(1);
 
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Details_Passes_All_Course_To_View()
+        {
+            var expectedCourses = new Courses();
+            coursesRepo.GetById(1).Returns(expectedCourses);
+
+            var result = underTest.Details(1);
+
+            Assert.Equal(expectedCourses, result.Model);
+        }
 
 
     }
---
+++ october_courses_workshop.Tests/UniversityMockTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace october_courses_workshop.Tests
+{
+    
+    class UniversityMockTests
+    {
+
+    }
+}
