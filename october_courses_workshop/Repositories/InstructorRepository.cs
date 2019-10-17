bf564c6139710329ef91cd1c7140fa6adf64949a
Jen E <jen@wecancodeit.org>
2019-10-17 09:29:54 -0400

    added instructor repository

---
+++ october_courses_workshop/Repositories/InstructorRepository.cs
@@ -0,0 +1,40 @@
+﻿using october_courses_workshop.Data;
+using october_courses_workshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace october_courses_workshop.Repositories
+{
+    public class InstructorRepository : IRepository<Instructor>
+    {
+        private UniversityContext db;
+
+        public InstructorRepository(UniversityContext db)
+        {
+            this.db = db;
+        }
+
+        public void Create(Instructor instructor)
+        {
+            db.Instructors.Add(instructor);
+            db.SaveChanges();
+        }
+
+        public void Delete(Instructor obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Instructor> GetAll()
+        {
+            return db.Instructors.ToList();
+        }
+
+        public Instructor GetById(int id)
+        {
+            return db.Instructors.Single(c => c.Id == id);
+        }
+    }
+}
