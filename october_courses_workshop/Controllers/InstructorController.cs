ff3e88e4b4983692324b402541f6c8b4fc66d026
Jen E <jen@wecancodeit.org>
2019-10-17 09:27:47 -0400

    added instructor controller

---
+++ october_courses_workshop/Controllers/InstructorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using october_courses_workshop.Models;
+using october_courses_workshop.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace october_courses_workshop.Controllers
+{
+    public class InstructorController : Controller
+    {
+
+        IRepository<Models.Instructor> instructorRepo;
+
+        public InstructorController(IRepository<Instructor> instructorRepo)
+        {
+            this.instructorRepo = instructorRepo;
+        }
+
+        public ViewResult Index()
+        {
+            var model = instructorRepo.GetAll();
+            return View(model);
+        }
+
+        public ViewResult Details(int id)
+        {
+            var model = instructorRepo.GetById(id);
+            return View(model);
+        }
+    }
+}
